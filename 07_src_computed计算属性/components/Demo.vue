4275fa321b12bfc2b274b6d275a161d5fc34bf70
yuting <1950876114@qq.com>
2024-01-12 14:17:55 +0800

    update

---
+++ 07_src_computed计算属性/components/Demo.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1>一个人的信息</h1>
+    <h2>姓名：</h2>
+    <input type="text" v-model="person.name">
+    <h2>年龄：</h2>
+    <input type="number" name="" id="" v-model="person.age">
+    <h2>{{ person.fullInfo }}</h2>
+    <h2>信息：</h2>
+    <input type="text" v-model="person.fullInfo">
+</template>
+<script>
+import { reactive, computed } from 'vue'
+export default {
+    name: 'Demo',
+    // vue2中的写法，Vue3向下兼容也可以实现
+    // computed: {
+    //     fullInfo() {
+    //         return `姓名：${this.person.name} 年龄：${this.person.age}`
+    //     }
+    // },
+    setup() {
+        let person = reactive({
+            name: '张三',
+            age: 18
+        })
+        //计算属性——简写(没有考虑计算属性被修改的情况)
+        person.fullInfo = computed(() => {
+            return `姓名：${person.name} 年龄：${person.age}`
+        })
+        //计算属性——完整
+        person.fullInfo = computed({
+            get() {
+                return `姓名：${person.name} 年龄：${person.age}`
+            },
+            set(val) {
+                person.name = '李四'
+                person.age = 25
+            }
+        })
+        return {
+            person
+        }
+    }
+}
+</script>
+<style>
+</style>
--- 08_src_watch函数/components/Demo.vue
+++ 08_src_watch函数/components/Demo.vue
@@ -15,24 +15,27 @@ export default {
     setup() {
         let sum = ref(0)
         let msg = ref('你好啊')
-        let person = ref({
+        let person = reactive({
             name: '张三',
             age: 18
         })
-        console.log(sum, 'sum');
-        console.log(person, 'person');
+        // 1.情况一：监视ref所定义的一个响应式数据
         watch(sum, (newVal, oldVal) => {
             console.log('sum变了', newVal, oldVal);
+        }, {
+            immediate: true
         })
-        
-        watch(person.value, (newVal, oldVal) => {
-            console.log('person变了', newVal, oldVal);
+        // 2.情况二：监视ref所定义的多个响应式数据
+        watch([sum, msg], (newVal, oldVal) => {
+            console.log('sum或msg变了', newVal, oldVal);
+        }, {
+            immediate: true
         })
-
+        // 3.情况三：监视reactive所定义的一个响应式数据，注意：此处无法正确的获取oldVal
         watch(person, (newVal, oldVal) => {
             console.log('person变了', newVal, oldVal);
         }, {
-            deep: true
+            immediate: true,
         })
 
         return {
